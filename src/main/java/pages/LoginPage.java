b08080d088b4526f0dfd6f31ca022810df6f4188
hv <hovhannes.vardanyan@picsart.com>
2021-05-16 20:31:23 +0400

    login delay

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -24,6 +24,7 @@ public class LoginPage {
         webDriver.findElement(imdbLoginField).sendKeys(login);
         webDriver.findElement(imdbPasswordField).sendKeys(password);
         webDriver.findElement(imdbSigninButton).click();
+        Thread.sleep(2000) // login delay
         return webDriver.findElements(authErrorWindow).size() == 0 && webDriver.findElements(warningErrorWindow).size() == 0;
     }
 
